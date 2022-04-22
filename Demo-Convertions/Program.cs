d5d405f60feebf78480c12b5b40fc60baa0216d3
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-22 10:59:57 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-Convertions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo_Convertions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez introduire un nombre : ");
+            string input = Console.ReadLine();
+            int nb1 = Convert.ToInt32(input);
+
+            Console.WriteLine("Veuillez entrer un second nombre : ");
+            input = Console.ReadLine();
+            int nb2 = int.Parse(input);
+
+            Console.WriteLine($"{nb1} + {nb2} = {nb1 + nb2}");
+        }
+    }
+}
