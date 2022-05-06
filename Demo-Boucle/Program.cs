1cfceafe5bf33ec37e8623cc21f12cce3773fc89
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-06 11:37:57 +0200

    Update Demo-Boucles

--- Demo-Boucle/Program.cs
+++ Demo-Boucle/Program.cs
@@ -29,10 +29,42 @@ namespace Demo_Boucle
                     nbTirret++;
                 }
                 Thread.Sleep(200); */
-                #endregion
+            #endregion
+            #region Boucle avec taille inconnue
+
+            //Console.WriteLine("Veuillez entrer un numéro BBAN (12 chiffres) :");
+            //string bban = Console.ReadLine();
+            ////while (bban.Length != 12 || !ulong.TryParse(bban, out _))
+            //while ( !(bban.Length == 12 && ulong.TryParse(bban, out _)))
+            //   {
+            //    Console.WriteLine("Les \"Coffee shrimps\" ont demandé d'avoir 12 chiffres :");
+            //    bban = Console.ReadLine();
+            //}
 
 
+            //string bban;
+            //do
+            //{
+            //    Console.WriteLine("Veuillez entrer un numéro BBAN (12 chiffres) :");
+            //    bban = Console.ReadLine();
+            //} while (bban.Length != 12 || !ulong.TryParse(bban, out _));
+
+            //string bban;
+            //string message = "Veuillez entrer un numéro BBAN (12 chiffres) :";
+            //do
+            //{
+            //    Console.WriteLine(message);
+            //    bban = Console.ReadLine();
+            //    message = "Les \"Coffee shrimps\" ont demandé d'avoir 12 chiffres :";
+            //} while (bban.Length != 12 || !ulong.TryParse(bban, out _));
+
+            Console.WriteLine("Veuillez entrer un numéro BBAN(12 chiffres) :");
+            for (string bban = Console.ReadLine(); bban.Length !=12 || !ulong.TryParse(bban, out _); bban = Console.ReadLine())
+            {
+                Console.WriteLine("Les \"Coffee shrimps\" ont demandé d'avoir 12 chiffres :");
             }
+            #endregion
+
         }
     }
 }
