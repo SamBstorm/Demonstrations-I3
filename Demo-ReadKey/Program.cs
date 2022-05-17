abcc4d675de6b6826e28b6d511b28d90fbc37934
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-17 10:10:34 +0200

    Add Demonstration Console.ReadKey()

---
+++ Demo-ReadKey/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo_ReadKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                Console.WriteLine("Veuillez entrer une combinaison de touches :");
+                keyInfo = Console.ReadKey(true);
+                Console.WriteLine(keyInfo.Key);
+                Console.WriteLine(keyInfo.Modifiers);
+            //} while (keyInfo.Modifiers != ConsoleModifiers.Shift || keyInfo.Key != ConsoleKey.Q);
+            } while (!(keyInfo.Modifiers == ConsoleModifiers.Shift && keyInfo.Key == ConsoleKey.Q));
+        }
+    }
+}
