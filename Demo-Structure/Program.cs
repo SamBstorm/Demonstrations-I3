2e2c31c6cbe775944f3677f9ce10bcc76167f96d
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-17 11:51:52 +0200

    Add Demonstration Structures

---
+++ Demo-Structure/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo_Structure
+{
+    public struct Donjon {
+        public ushort difficulte;
+        public List<Etage> etages;
+    }
+
+    public struct Etage {
+        public int nbEnnemi;
+        public string loot;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Donjon donjon1;
+            donjon1.difficulte = 1;
+            donjon1.etages = new List<Etage>();
+            Console.WriteLine(donjon1.difficulte);
+            donjon1.etages.Add(new Etage() { nbEnnemi = 3, loot = "Clé donjon" });
+
+            if (donjon1.etages[0].nbEnnemi <= 0) {
+                Etage e1 = donjon1.etages[0];
+                e1.loot = "Déjà looté!";
+                donjon1.etages[0] = e1;
+            }
+
+            Donjon donjon2 = new Donjon();
+            Console.WriteLine(donjon2.difficulte);
+        }
+    }
+}
