121591f99c02e20f6d55c5adac33f2861543ecf8
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-18 09:49:05 +0200

    Add Demonstration Méthodes

---
+++ Demo-Methode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Demo_Methode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleAvance ca;
+            ca.ArrayWriteLine();
+        }
+    }
+}
---
+++ Demo-Methode/Structs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Methode
+{
+    public struct ConsoleAvance { 
+        
+        public void RainbowWriteLine()
+        {
+            string message = "Hello rainbowWorld!";
+            for (int i = 0; i < message.Length; i++)
+            {
+                int dif = 15 - 9;
+                Console.ForegroundColor = (ConsoleColor)((i % dif) + 9);
+                Console.Write(message[i]);
+            }
+        }
+
+        public void ArrayWriteLine()
+        {
+            string[] array = { "Virginie", "Shaila", "Resa", "Alina", "Charlotte", "Naïké", "Julie", "Vanessa", "Manon", "Zineb", "Scheherazad", "Cristine", "Hajar" };
+            Console.Write("[");
+            foreach (string arrayElement in array)
+            {
+                Console.Write($"{arrayElement},");
+            }
+            Console.Write("\b]");
+            Console.WriteLine();
+        }
+
+    }
+
+}
