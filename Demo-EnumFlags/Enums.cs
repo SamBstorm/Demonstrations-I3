82e7dd2cc113ea536ff7c4b2a99e866d8b21db44
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-24 13:42:48 +0200

    Add Démonstration Enumaration avec flags

---
+++ Demo-EnumFlags/Enums.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_EnumFlags
+{
+    [Flags]
+    public enum StatusPokemon { Poison = 1, Paralise = 2, Endormi = 4, Gele = 8, Brule = 16, KO = 32}
+}
---
+++ Demo-EnumFlags/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Demo_EnumFlags
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StatusPokemon poke1 = StatusPokemon.Brule | StatusPokemon.Endormi;
+
+            if (poke1.HasFlag(StatusPokemon.Poison)) Console.WriteLine("Il est empoisonné...");
+            if (poke1.HasFlag(StatusPokemon.Paralise)) Console.WriteLine("Il est paralisé...");
+            if (poke1.HasFlag(StatusPokemon.Endormi)) Console.WriteLine("Il est endormi...");
+            if (poke1.HasFlag(StatusPokemon.Gele)) Console.WriteLine("Il est gelé...");
+            if (poke1.HasFlag(StatusPokemon.Brule)) Console.WriteLine("Il est brûlé...");
+            if (poke1.HasFlag(StatusPokemon.KO)) Console.WriteLine("Il ne peut plus se battre...");
+
+        }
+    }
+}
